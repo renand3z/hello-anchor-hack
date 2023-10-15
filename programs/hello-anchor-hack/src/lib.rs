97d0758aaa12b2852a3aca07a93dafa4fa79f03f
renan <renand3z@gmail.com>
2023-10-15 00:16:10 -0300

    counter working with tests

--- programs/hello-anchor-hack/src/lib.rs
+++ programs/hello-anchor-hack/src/lib.rs
@@ -11,19 +11,66 @@ pub struct Hello {
 pub mod hello_anchor_hack {
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
-        msg!("we're all in our private traps");
+    pub fn initialize(ctx: Context<CreateCounter>) -> Result<()> {
+        msg!("Creating a Counter!!");
+
+        // The creation of the counter must be here
+        let counter = &mut ctx.accounts.counter;
+        counter.authority = ctx.accounts.authority.key();
+        counter.count = 0;
+
+        
+        msg!("Current count is {}", counter.count);
+        msg!("The Admin PubKey is: {} ", counter.authority);
+
         Ok(())
     }
 
-    pub fn initialize2(_ctx: Context<Initialize>) -> Result<()> {
-        msg!("we're all have a choice");
+    pub fn initialize2(ctx: Context<UpdateCounter>) -> Result<()> {
+        msg!("Adding 1 to the counter!!");
+
+        // Updating the counter must be here 
+        let counter = &mut ctx.accounts.counter;
+        counter.authority = ctx.accounts.authority.key();
+        counter.count += 1;
+
+        msg!("Current count is {}", counter.count);
+        msg!("{} remaining to reach 1000 ", 1000 - counter.count);
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+// #[derive(Accounts)]
+// pub struct Initialize {}
+
+// #[derive(Accounts)]
+// pub struct Initialize2 {}
 
 #[derive(Accounts)]
-pub struct Initialize2 {}
+pub struct CreateCounter<'info> {
+    #[account(mut)]
+    authority: Signer<'info>,
+    #[account(
+        init,
+        seeds = [authority.key().as_ref()],
+        bump,
+        payer = authority,
+        space = 100
+    )]
+    counter: Account<'info, Counter>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateCounter<'info> {
+    authority: Signer<'info>,
+    #[account(mut, has_one = authority)]
+    counter: Account<'info, Counter>,
+}
+
+// Data structures
+#[account]
+pub struct Counter {
+    authority: Pubkey,
+    count: u64,
+}
--- tests/hello-anchor-hack.ts
+++ tests/hello-anchor-hack.ts
@@ -7,30 +7,86 @@ describe("hello-anchor-hack", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.HelloAnchorHack as Program<HelloAnchorHack>;
+  const systemProgram = anchor.web3.SystemProgram;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+
+  it("Create Counter!", async () => {
+    // Keypair = account
+    const [counter, _counterBump] =
+      await anchor.web3.PublicKey.findProgramAddress(
+        [program.provider.publicKey.toBytes()],
+        program.programId
+      );
+    console.log("Your counter address", counter.toString());
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        authority: program.provider.publicKey,
+        counter: counter,
+        systemProgram: systemProgram.programId,
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 
-  it("Is initialized2!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize2().rpc();
-    console.log("Your transaction signature", tx);
+  it("Fetch a counter!", async () => {
+    // Keypair = account
+    const [counterPubkey, _] = await anchor.web3.PublicKey.findProgramAddress(
+      [program.provider.publicKey.toBytes()],
+      program.programId
+    );
+    console.log("Your counter address", counterPubkey.toString());
+    const counter = await program.account.counter.fetch(counterPubkey);
+    console.log("Your counter", counter);
   });
+
+  it("Update a counter!", async () => {
+    // Keypair = account
+    const [counterPubkey, _] = await anchor.web3.PublicKey.findProgramAddress(
+      [program.provider.publicKey.toBytes()],
+      program.programId
+    );
+    console.log("Your counter address", counterPubkey.toString());
+    const counter = await program.account.counter.fetch(counterPubkey);
+    console.log("Your counter", counter);
+    const tx = await program.methods
+      .initialize2()
+      .accounts({
+        counter: counterPubkey,
+      })
+      .rpc();
+    console.log("Your transaction signature", tx);
+    const counterUpdated = await program.account.counter.fetch(counterPubkey);
+    console.log("Your counter count is: ", counterUpdated.count.toNumber());
+  });
+
+
+  // it("Is initialized!", async () => {
+  //   await program.methods
+  //     .initialize()
+  //     .accounts({
+  //       counter: counterKeypair.publicKey,
+  //       payer: payer.publicKey,
+  //     })
+  //     .signers([counterKeypair])
+  //     .rpc()
+
+  //   const currentCount = await program.account.counter.fetch(
+  //     counterKeypair.publicKey
+  //   )
+  //   // Add your test here.
+  //   const [counter, _counterBump] = await program.methods.initialize().rpc();
+  //   console.log("Your transaction signature", tx);
+
+  //   console.log(
+  //     currentCount.count.toNumber(),
+  //     "Expected initialized count to be 0"
+  //   )
+  // });
+
+  // it("Is initialized2!", async () => {
+  //   // Add your test here.
+  //   const tx = await program.methods.initialize2().rpc();
+  //   console.log("Your transaction signature", tx);
+  // });
 });
-
-// import * as anchor from "@coral-xyz/anchor";
-// import type { HelloAnchorHack } from "../target/types/hello_anchor_hack";
-
-// // Configure the client to use the local cluster
-// anchor.setProvider(anchor.AnchorProvider.env());
-
-// const program = anchor.workspace.HelloAnchorHack as anchor.Program<HelloAnchorHack>;
-
-// // Client code...
-// console.log(program.programId.toString());
-
-// const txHash = program.methods.initialize().rpc();
-// console.log(txHash);
